58aeba96469bef075b336c9a2a841281f92cb2c2
Wolfsmirks <Jacob.Guest.0@gmail.com>
2025-07-25 16:48:17 +0100

    Configure production connection

--- db/connection.js
+++ db/connection.js
@@ -4,12 +4,19 @@ const ENV = process.env.NODE_ENV || "development";
 
 require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
-const db = new Pool();
+//const db = new Pool();
 
-if (!process.env.PGDATABASE) {
-  throw new Error("No PGDATABASE configured");
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("No PGDATABASE or DATABASE_URL configured");
 } else {
   console.log(`Connected to ${process.env.PGDATABASE}`);
 }
 
-module.exports = db;
+const config = {};
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+module.exports = new Pool(config);
