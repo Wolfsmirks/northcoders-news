d398e1096ea5342303c24f96a6b27449e674728a
Wolfsmirks <Jacob.Guest.0@gmail.com>
2025-07-25 17:21:58 +0100

    Remove a console.log

--- db/connection.js
+++ db/connection.js
@@ -6,11 +6,8 @@ require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
 //const db = new Pool();
 
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL)
   throw new Error("No PGDATABASE or DATABASE_URL configured");
-} else {
-  console.log(`Connected to ${process.env.PGDATABASE}`);
-}
 
 const config = {};
 
