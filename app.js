8d83c63177af3a2732cf7b48a186f1ae7e7e96dd
Wolfsmirks <Jacob.Guest.0@gmail.com>
2025-07-30 10:07:34 +0100

    Add index.html for API documentation

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+app.use("/api", express.static("public"));
 app.use("/api", apiRouter);
 
 app.use(handleBadRequest);
