0f40913ac038c50c1d1c77cbecf425a5d01bf283
Wolfsmirks <Jacob.Guest.0@gmail.com>
2025-08-11 12:01:51 +0100

    Add CORS

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const {
   handleServerError,
 } = require("./errors");
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api", express.static("public"));
 app.use("/api", apiRouter);
